8f01f6e617c7a79506a9c68c057abf8a9544a5bf
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-06-12 00:38:32 -0300

    Update MainMenuObserver.cpp

--- src/Observer/MainMenuObserver.cpp
+++ src/Observer/MainMenuObserver.cpp
@@ -16,8 +16,43 @@ MainMenuObserver::~MainMenuObserver()
 }
 void MainMenuObserver::PressedKey(const sf::Keyboard::Key key)
 {
+    // switch (key)
+    // {
+    // case sf::Keyboard::Enter:
+    //     switch (mainmenu->GetIDSelectedButton())
+    //     {
+    //     case (ID::ID::SinglePlayer_Menu):
+    //         pStateMa->pushState(ID::ID::Level_Select_Menu, false);
+    //         break;
+    //     case (ID::ID::Multiplayer_Menu):
+    //         pStateMa->pushState(ID::ID::Level_Select_Menu, true);
+    //         break;
+    //     case (ID::ID::Ranking_Menu):
+    //         pStateMa->pushState(ID::ID::Ranking_Menu, true);
+    //         break;
+    //     case (ID::ID::exit_button):
+    //         pStateMa->popState();
+    //         break;
+    //     default:
+    //         break;
+    //     }
+    //     break;
+
+    // default:
+    //     break;
+    // }
+}
+
+void Whispers::Observer::MainMenuObserver::ReleasedKey(const sf::Keyboard::Key key)
+{
     switch (key)
     {
+    case (sf::Keyboard::Up):
+        mainmenu->SelectAbove();
+        break;
+    case (sf::Keyboard::Down):
+        mainmenu->SelectBelow();
+        break;
     case sf::Keyboard::Enter:
         switch (mainmenu->GetIDSelectedButton())
         {
@@ -33,26 +68,7 @@ void MainMenuObserver::PressedKey(const sf::Keyboard::Key key)
         case (ID::ID::exit_button):
             pStateMa->popState();
             break;
-        default:
-            break;
         }
-        break;
-
-    default:
-        break;
-    }
-}
-
-void Whispers::Observer::MainMenuObserver::ReleasedKey(const sf::Keyboard::Key key)
-{
-    switch (key)
-    {
-    case (sf::Keyboard::Up):
-        mainmenu->SelectAbove();
-        break;
-    case (sf::Keyboard::Down):
-        mainmenu->SelectBelow();
-        break;
     default:
         break;
     }
